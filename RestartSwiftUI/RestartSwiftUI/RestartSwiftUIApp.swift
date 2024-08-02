632c9d55df19efd9c32b4bdc8b0e8d7892eccfa6
Shiva Sai Netha Rudra <shivasainetha.rudra@gmail.com>
2024-08-02 01:32:52 -0700

    Project is created

---
+++ RestartSwiftUI/RestartSwiftUI/RestartSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartSwiftUIApp.swift
+//  RestartSwiftUI
+//
+//  Created by Shiva Sai Netha Rudra on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
