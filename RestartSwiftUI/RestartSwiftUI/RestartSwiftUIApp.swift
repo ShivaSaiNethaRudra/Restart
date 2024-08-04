331b8a2e897441a09db0a208dda5d8c7f10b4f76
Shiva Sai Netha Rudra <shivasainetha.rudra@gmail.com>
2024-08-04 00:01:10 -0700

    Restart App is ready running

---
+++ RestartSwiftUI/RestartSwiftUI/RestartSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartSwiftUIApp.swift
+//  RestartSwiftUI
+//
+//  Created by Shiva Sai Netha Rudra on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
